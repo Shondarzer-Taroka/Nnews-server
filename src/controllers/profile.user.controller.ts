61c9155f607041c3d6e71a03d11962c101152e91
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-07-14 12:46:50 +0600

    profile user get

--- src/controllers/profile.user.controller.ts
+++ src/controllers/profile.user.controller.ts
@@ -5,10 +5,41 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 
-export const singleUserInfo=async (req:Request,res:Response) => {
+export const getSingleUserInfo = async (req: Request, res: Response):Promise<any>=> {
     try {
+        const { id } = req.params
+        console.log(req.params,'profile');
         
+        if (!id) {
+            return res.status(404).json({ message: 'Not found id' })
+        }
+
+        const user = await prisma.user.findUnique({ where: { id } })
+
+        if (!user) {
+            return res.status(404).json({ message: 'Not found user' })
+        }
+
+        res.json({
+            message: 'user found successfully',
+            user: {
+                name: user?.name,
+                email: user?.email,
+                role: user?.role,
+                image: user?.image,
+                address: user?.address,
+                district: user?.district,
+                division: user?.division,
+                thana: user?.thana,
+                union: user?.union,
+                postCode: user?.postCode
+            }
+
+
+
+        })
     } catch (error) {
-        
+        console.error(error);
+        res.status(401).json({ message: 'forgotten id' });
     }
 }
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import passport from 'passport';
 import { register, login, logInUser, logout } from '../controllers/user.controller';
+import { getSingleUserInfo } from '../controllers/profile.user.controller';
 
 const router = express.Router();
 
@@ -8,6 +9,7 @@ router.post('/register', register);
 router.post('/login', passport.authenticate('local', { session: false }), login);
 router.post('/logout', logout);
 router.get('/logInUser',logInUser)
+router.get('/singleUserInfo/:id',getSingleUserInfo)
 export default router;
 
 // router.get('/logInUser',logInUser)
