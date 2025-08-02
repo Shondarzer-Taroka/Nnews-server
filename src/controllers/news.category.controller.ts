a78b533c8ad7d6ebbc374ad9770a580616df76a7
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-08-02 12:36:53 +0600

    page number and limitation added

--- src/controllers/news.category.controller.ts
+++ src/controllers/news.category.controller.ts
@@ -22,6 +22,9 @@ export const getNewsArea = async (req: Request, res: Response) => {
             page,
             limit
         } = req.query as unknown as QueryParams
+        const pageNumber=Number(page)|| 1
+        const limitNumber=Number(limit)|| 10
+        
     } catch (error) {
 
     }
